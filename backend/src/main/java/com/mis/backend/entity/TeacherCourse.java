da660b9dfde038058fe72d026a553c0f91ac1e3a
Aer-IT <1269938269@qq.com>
2025-05-14 00:41:19 +0800

    5.13 commit

--- backend/src/main/java/com/mis/backend/entity/TeacherCourse.java
+++ backend/src/main/java/com/mis/backend/entity/TeacherCourse.java
@@ -1,6 +1,5 @@
 package com.mis.backend.entity;
 
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -51,9 +50,6 @@ public class TeacherCourse implements Serializable {
      * 更新时间
      */
     private LocalDateTime updateTime;
-
-    @TableLogic
-    private Integer deleted;
 
 
 }
